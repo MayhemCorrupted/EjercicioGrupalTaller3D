f7ed03b2f11bc1bb826671497e602743f428a378
Mayhem <alexandersaca3@gmail.com>
2026-04-16 18:20:32 -0500

    Valores de enemigo
    Se añadió tanto la vida como el daño para acceder

---
+++ EnemigoValores.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioGrupalTaller3D
+{
+    internal abstract class EnemigoValores
+    {
+        protected int damage;
+        protected int health;
+        public EnemigoValores(int damage, int health)
+        {
+            this.damage = damage;
+            this.health = health;
+        }
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+        }   
+        public int TakeHealth() => health;
+        public bool IsDead() => health <= 0;
+    }
+}
